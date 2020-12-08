0615d31b4f62da5b33485a112469975f7c7a698c
NtdChnn <69079640+NtdChnn@users.noreply.github.com>
2020-12-08 17:22:14 +0700

    PlayerHitBox !

--- DinoisEvo01/PlayerHitBox.cpp
+++ DinoisEvo01/PlayerHitBox.cpp
@@ -1,1 +1,14 @@
 #include "PlayerHitBox.h"
+PlayerHitBox::PlayerHitBox(Vector2f size)
+{
+	playerHitBox.setSize(size);
+}
+
+PlayerHitBox::~PlayerHitBox()
+{
+}
+
+void PlayerHitBox::Update(Vector2f playerposition)
+{
+	playerHitBox.setPosition(playerposition);
+}
--- DinoisEvo01/PlayerHitBox.h
+++ DinoisEvo01/PlayerHitBox.h
@@ -1,5 +1,15 @@
 #pragma once
+#include <SFML\Graphics.hpp>
+using namespace sf;
 class PlayerHitBox
 {
+public:
+	PlayerHitBox( Vector2f size );
+	~PlayerHitBox();
+
+	FloatRect GetGlobalBounds() { return playerHitBox.getGlobalBounds(); };
+	void Update(Vector2f playerposition);
+private:
+	RectangleShape playerHitBox; 
 };
 
